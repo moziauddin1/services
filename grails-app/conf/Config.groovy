35228c12c4174373929eaa11313d2be9a59af1b7
pmcneil <peter@nerderg.com>
2015-12-17 09:19:42 +1100

    comment out the blockcache config

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -216,31 +216,32 @@ grails.cache.config = {
         diskExpiryThreadIntervalSeconds 600
         memoryStoreEvictionPolicy 'LRU'}
 
-    cache {
-        name 'apniblockcache'
-        maxElementsInMemory 10000
-        eternal false
-        timeToIdleSeconds 120
-        timeToLiveSeconds 120
-        overflowToDisk true
-        maxElementsOnDisk 10000000
-        diskPersistent false
-        diskExpiryThreadIntervalSeconds 120
-        memoryStoreEvictionPolicy 'LRU'
-    }
-
-    cache {
-        name 'apcblockcache'
-        maxElementsInMemory 10000
-        eternal false
-        timeToIdleSeconds 120
-        timeToLiveSeconds 120
-        overflowToDisk true
-        maxElementsOnDisk 10000000
-        diskPersistent false
-        diskExpiryThreadIntervalSeconds 120
-        memoryStoreEvictionPolicy 'LRU'
-    }
+    //currently not used while we work out eviction policy
+//    cache {
+//        name 'apniblockcache'
+//        maxElementsInMemory 10000
+//        eternal false
+//        timeToIdleSeconds 120
+//        timeToLiveSeconds 120
+//        overflowToDisk true
+//        maxElementsOnDisk 10000000
+//        diskPersistent false
+//        diskExpiryThreadIntervalSeconds 120
+//        memoryStoreEvictionPolicy 'LRU'
+//    }
+//
+//    cache {
+//        name 'apcblockcache'
+//        maxElementsInMemory 10000
+//        eternal false
+//        timeToIdleSeconds 120
+//        timeToLiveSeconds 120
+//        overflowToDisk true
+//        maxElementsOnDisk 10000000
+//        diskPersistent false
+//        diskExpiryThreadIntervalSeconds 120
+//        memoryStoreEvictionPolicy 'LRU'
+//    }
 
     defaultCache {
         maxElementsInMemory 10000
