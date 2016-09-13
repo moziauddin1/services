5b9f6c868427c7ddb8e5bed4825e9eebda5f7f92
pmcneil <peter@nerderg.com>
2016-09-13 15:07:12 +1000

    Add a trait to get the remote IP address from headers and log who is doing search requests

--- grails-app/controllers/au/org/biodiversity/nsl/api/RequestUtil.groovy
+++ grails-app/controllers/au/org/biodiversity/nsl/api/RequestUtil.groovy
@@ -12,14 +12,12 @@ trait RequestUtil {
     private final ArrayList<String> headers = ["X-Forwarded-For","Proxy-Client-IP","WL-Proxy-Client-IP","HTTP_CLIENT_IP","HTTP_X_FORWARDED_FOR"]
 
     public remoteAddress(HttpServletRequest request) {
-        String ip = headers.find { String header ->
-            String value = request.getHeader(header)
-            if (value && !"unknown".equalsIgnoreCase(value)) {
-                return value
-            }
-            return null
+        String ip = null
+        String headerFound = headers.find { String header ->
+            ip = request.getHeader(header)
+            return ip && !"unknown".equalsIgnoreCase(ip)
         }
-        if (!ip) {
+        if (!headerFound) {
             ip = request.getRemoteAddr();
         }
         return ip;
